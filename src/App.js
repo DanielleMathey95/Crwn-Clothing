0eba141491ff7e2e960b2873c454c07e64bcdf3d
DanielleMathey95 <danielle.mathey@yahoo.com>
2021-01-17 18:03:47 -0800

    init commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return <div className='App'>Hello World</div>;
-  }
+function App() {
+  return (
+    <div>
+      hello world
+    </div>
+  )
 }
-
-export default App;
